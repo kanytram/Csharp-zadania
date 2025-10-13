0c14a3ebd887a85f5cc4af879d25cf0d0a4c7f17
martyna <xyz.mav@gmail.com>
2025-10-13 21:10:45 +0200

    1 zadanie

---
+++ zadanie1/zadanie1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+class Program
+
+{
+    static void Main()
+    {
+        Console.Write("Podaj swój wiek: ");
+
+        int wiek;
+
+        while (true) {
+
+            try
+            {
+                wiek = Convert.ToInt32(Console.ReadLine()); 
+                break; 
+            }
+
+            catch (Exception wyjatek)
+            {
+                Console.WriteLine("Błędne dane, spróbuj jeszcze raz");
+            }
+        }
+
+        string answer;
+
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("Czy masz legitymację? Wpisz 'tak' lub 'nie'");
+                answer = Console.ReadLine().ToUpper();
+
+                if (answer == "TAK" || answer == "NIE")
+                {
+                    break;
+                }
+                throw new Exception();
+            }
+
+            catch (Exception wyjatek1)
+            {
+                Console.WriteLine("Błędne dane, spróbuj jeszcze raz");
+            }
+
+        }
+
+        bool hasId = (answer == "TAK") ? true : false;
+
+        bool wstep = (wiek >= 18 && hasId) ? true : false;
+
+        if (wstep) Console.WriteLine("Wstęp dozwolony");
+
+        else Console.WriteLine("Wstęp NIEdozwolony");
+     }
+}
+
+
