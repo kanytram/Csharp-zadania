aa070ccbf6dcd40e545908198af77e7322368998
martyna <xyz.mav@gmail.com>
2025-10-18 13:03:51 +0200

    2 i 3 zadanie

---
+++ zadanie 3/zadanie 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static int[] tablica = new int[4] { 1, 2, 3, 4 };
+    public static void Zamiana(int a, int b)
+    {
+        int bufor;
+        bufor = tablica[a];
+        tablica[a] = tablica[b];
+        tablica[b] = bufor;       
+
+    }
+    static void Main()
+    {
+        
+
+        Console.Write(tablica[0] + " ");
+        Console.Write(tablica[1] + " ");
+        Console.Write(tablica[2] + " ");
+        Console.WriteLine(tablica[3] + " ");
+
+        Zamiana(0, 3);
+
+        Console.WriteLine(tablica[0]);
+        Console.WriteLine(tablica[1]);
+        Console.WriteLine(tablica[2]);
+        Console.WriteLine(tablica[3]);
+    }
+}
---
+++ zadanie2/zadanie2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 6;
+        int b = 10;
+
+        Console.WriteLine(a & b);
+        Console.WriteLine(a | b);
+        Console.WriteLine(a ^ b);
+        Console.WriteLine(a << 1);
+        Console.WriteLine(b >> 2);
+    }
+}
